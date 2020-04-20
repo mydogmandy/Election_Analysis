f2f5f602b4a71b9e4497536128428b9089407f6c
mydogmandy <62800249+mydogmandy@users.noreply.github.com>
2020-04-19 23:47:10 -0500

    Adding the election analysis

---
+++ Module.py
@@ -0,0 +1,11 @@
+candidate_votes = int(input("How many votes did you get in the election? "))
+total_votes = int(input("What is the total votes in the election? "))
+
+#percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+#print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+message_to_candidate = (
+    f'You received {candidate_votes} number of votes.  '
+    f'The total number of votes in the election was {total_votes}.  '
+    f'You received {candidate_votes / total_votes * 100:.1f}% of the total')
+print(message_to_candidate)
