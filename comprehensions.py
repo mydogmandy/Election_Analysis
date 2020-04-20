5d56e8085c626c6673f5df158ab951fbf5f82343
mydogmandy <62800249+mydogmandy@users.noreply.github.com>
2020-04-19 23:56:06 -0500

    Delete comprehensions.py

--- comprehensions.py
+++
@@ -1,9 +0,0 @@
-# How many votes did you get?
-# Total votes in the election
-# Calculate the percentage of votes you received
-
-my_votes = float(input('How many votes did you get in the election?'))
-total_votes = float(input('What is the total votes in the election?'))
-percentage_votes = round(((my_votes / total_votes) * 100),2)
-print('I got ' +str(percentage_votes)+'% of the votes')
-
